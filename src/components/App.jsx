7f7e5dca78a15a04612a50977119be0f7ad7b5a7
Igor2109 <127054357+Igor2109@users.noreply.github.com>
2023-10-01 20:54:09 +0200

    local

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,18 @@ export class App extends Component {
     ],
     filter: '',
   };
-
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts')
+    if (savedContacts) {
+      this.setState({contacts:JSON.parse(savedContacts)})
+    }
+  }
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts.length !== prevState.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+  
   handleAddContact = userContacts => {
     if (
       this.state.contacts.some(
